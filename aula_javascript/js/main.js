e9f70cfe658413b0d3e07a2f60f7f5f93ae2b932
Lucas Vasconcelos <64870228+lucaskaufpatu@users.noreply.github.com>
2020-06-22 14:07:19 +0100

    Add files via upload

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,99 @@
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"))
+    //alert("Obrigado por clicar");
+}
+*/
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = ("Obrigado por passar o mouse");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+funciton voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = ("Passe o mouse aqui");
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento)
+    console.log(elemento.value)
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*
+var d = new Date();
+alert(d.getDate());
+alert(d.getDay());
+alert(d.getHours);
+*/
+
+
+/*
+var count;
+for(count=0; count <=  5; count++);{
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <=5){
+    console.log(count);
+    alert(count);
+    count ++;
+};
+*/
+
+
+
+/*
+var idade = prompt("qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+};
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var lista =["maça", "pêra", "laranja"];
+//lista.pop("");
+//lista.push("uva");
+console.log(lista);
+console.log(lista.toString())
+console.log(lista.join(" - "))
+*/
+
+//var nome = "Lucas Kafumann";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert (nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase);
+//alert(frase.replace("Japão","Brasil"));
